ddfee65c8d16c184d469886ac51d8e7b08231643
Marcos Segantine <msegantine8@gmail.com>
2022-11-21 09:01:24 -0300

    Styling new Footer

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -10,19 +10,31 @@ import linkedinIcon from "../images/icons/linkedin-icon.png";
 export default function Footer() {
   return (
     <div className={footer.footer}>
+      <address>
+        <h5>Email</h5>
+        <p>msegantine8@gmail.com</p>
+
+        <h5>Celular</h5>
+        <p>{"(34)"} 99679-2346</p>
+      </address>
+
       <section className={footer.networks}>
         <Link href="https://github.com/Marcos-Segantine">
           <a>
-            <Image src={githubIcon} width={35} height={35} alt="" />
+            <Image src={githubIcon} width={45} height={45} alt="" />
           </a>
         </Link>
 
         <Link href="https://www.linkedin.com/in/marcossegantine/">
           <a>
-            <Image src={linkedinIcon} width={35} height={35} alt="" />
+            <Image src={linkedinIcon} width={45} height={45} alt="" />
           </a>
         </Link>
-        <Image src={instagramIcon} width={35} height={35} alt="" />
+        <Link href="">
+          <a>
+            <Image src={instagramIcon} width={45} height={45} alt="" />
+          </a>
+        </Link>
       </section>
     </div>
   );
