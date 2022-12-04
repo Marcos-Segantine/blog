68a857182eb64eab5fae5c34f779474d66a28ebc
Marcos Segantine <msegantine8@gmail.com>
2022-12-03 22:02:22 -0300

    Fixing possible bugs

--- src/components/ArticlesLanguages.jsx
+++ src/components/ArticlesLanguages.jsx
@@ -14,14 +14,14 @@ export const ArticlesLanguages = ({ data }) => {
             <h4 className={articlesLanguages.articles_languages__language}>JavaScript</h4>
             <div className={articlesLanguages.articles_languages__content}>
                 {
-                    dataJavaScript.map(data => {
-                        return(
-                            <Link key={data.id} href={data.articles_url} className={articlesLanguages.articles_languages__content__article}>
-                                    <Tags data={data.tags} />
-                                    <h3>{data.title}</h3>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sequi autem sit itaque rem tempore quidem, hic exercitationem numquam minima quia quod iure voluptate laborum, molestias sed dignissimos illum necessitatibus?</p>
-                                    <span className={articlesLanguages.articles_languages__content__article__date}>{data.createdAt}</span>
-                            </Link>
+                dataJavaScript.map(data => {
+                    return(
+                        <Link key={data.id} href={data.articles_url} className={articlesLanguages.articles_languages__content__article}>
+                            <Tags data={data.tags} />
+                            <h3>{data.title}</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sequi autem sit itaque rem tempore quidem, hic exercitationem numquam minima quia quod iure voluptate laborum, molestias sed dignissimos illum necessitatibus?</p>
+                            <span className={articlesLanguages.articles_languages__content__article__date}>{data.createdAt}</span>
+                        </Link>
                         )
                     })
                 }
@@ -30,7 +30,8 @@ export const ArticlesLanguages = ({ data }) => {
             <h4 className={articlesLanguages.articles_languages__language}>ReactJs</h4>
             <div className={articlesLanguages.articles_languages__content}>
                 {
-                    dataReact.map(data => {
+                    dataReact.map((data, index) => {
+                        if(index > 3) return
                         return(
                             <Link key={data.id} href={data.articles_url} className={articlesLanguages.articles_languages__content__article}>
                                 <Tags data={data.tags} />
