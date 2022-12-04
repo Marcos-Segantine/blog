fd08fdd21abb110357bcc8faca99c7265da9af27
Marcos Segantine <msegantine8@gmail.com>
2022-12-04 19:32:07 -0300

    Resolve some issue

--- src/pages/api/addVisitArticle.js
+++ src/pages/api/addVisitArticle.js
@@ -2,7 +2,7 @@ import db from "./database/db";
 
 export default async function addVisitArticle(req, res) {
   db.query(
-    `UPDATE articles SET visits=${2} WHERE id=2;`,
+    `UPDATE articles SET visits=${2} WHERE id=${parseInt(res[0].visits) + 1};`,
     (err, result) => {
       res.status(200).json(result);
     }
