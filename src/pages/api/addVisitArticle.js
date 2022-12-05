a7cfe7a6f4bf1b9243c5b05a4573ab252d716819
Marcos Segantine <msegantine8@gmail.com>
2022-12-04 21:15:27 -0300

    Adding response status at route

--- src/pages/api/addVisitArticle.js
+++ src/pages/api/addVisitArticle.js
@@ -1,10 +1,12 @@
 import db from "./database/db";
 
 export default async function addVisitArticle(req, res) {
-  // const { article_url } = req.query
+  const { article_url } = req.query
 
   db.query(
-    `UPDATE articles SET visits=10 WHERE articles_url="hoisting-no-javascript-var-let-e-const";`
-  
+    `UPDATE articles SET visits=20 WHERE articles_url="hoisting-no-javascript-var-let-e-const";`,
+    (err, result) => {
+      result.status(200);
+    }
   );
 }
