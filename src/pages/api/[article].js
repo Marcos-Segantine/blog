beb70099dc7ae8b76446ca9555e406e31121a08a
Marcos Segantine <msegantine8@gmail.com>
2022-12-07 14:58:40 -0300

    Fixing route

--- src/pages/api/[article].js
+++ src/pages/api/[article].js
@@ -2,11 +2,10 @@ import db from "./database/db";
 
 export default async function handler(req, res) {
   const { article } = req.query
-  let visitsNumber = 0
 
   db.query(
-    "UPDATE articles SET visits=?;", [Visit]) 
-    
+    "UPDATE articles SET visits=30;") 
+
   db.query("SELECT * FROM articles WHERE articles_url=?", [article], (err, result) => {
     res.status(200).json(result);
   });
