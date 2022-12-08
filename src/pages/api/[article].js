a5a5c5f28425a5e2168c89374b3250c9d8278422
Marcos Segantine <msegantine8@gmail.com>
2022-12-08 09:16:03 -0300

    Improving inserction of visits

--- src/pages/api/[article].js
+++ src/pages/api/[article].js
@@ -2,7 +2,11 @@ import db from "./database/db";
 
 export default async function handler(req, res) {
   const { article } = req.query
-  let visits = 90
+  let visits = 1
+
+  db.query("SELECT visits FROM articles WHERE articles_url=?", [article], (err, result) => {
+    visits += result
+  })
 
   db.query("UPDATE articles SET visits=? WHERE articles_url=?", [visits, article]) 
 
