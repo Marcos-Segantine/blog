75539064d8e6feb016162611413f01a708b0c233
Marcos Segantine <msegantine8@gmail.com>
2022-12-08 09:07:24 -0300

    Adding new visits using variable

--- src/pages/api/[article].js
+++ src/pages/api/[article].js
@@ -2,8 +2,9 @@ import db from "./database/db";
 
 export default async function handler(req, res) {
   const { article } = req.query
+  let visits = 90
 
-  db.query("UPDATE articles SET visits=1 WHERE articles_url=?", [article]) 
+  db.query("UPDATE articles SET visits=? WHERE articles_url=?", [visits, article]) 
 
   db.query("SELECT * FROM articles WHERE articles_url=?", [article], (err, result) => {
     res.status(200).json(result);
