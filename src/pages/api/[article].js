9c79c828970597d92f74602d38426fe667f14f50
Marcos Segantine <msegantine8@gmail.com>
2022-12-08 09:42:19 -0300

    Improving API

--- src/pages/api/[article].js
+++ src/pages/api/[article].js
@@ -2,13 +2,13 @@ import db from "./database/db";
 
 export default async function handler(req, res) {
   const { article } = req.query
-  let visits = 1
 
   db.query("SELECT visits FROM articles WHERE articles_url=?", [article], (err, result) => {
-    visits += Number(result)
-  })
+    const atualVisits = Number(result)
+    const visits = atualVisits + visits  
 
-  db.query("UPDATE articles SET visits=? WHERE articles_url=?", [visits, article]) 
+    db.query("UPDATE articles SET visits=? WHERE articles_url=?", [visits, article]) 
+  })
 
   db.query("SELECT * FROM articles WHERE articles_url=?", [article], (err, result) => {
     res.status(200).json(result);
