1d885904090a95f8af1210a1aae858077217a80d
Marcos Segantine <msegantine8@gmail.com>
2022-12-08 08:51:15 -0300

    Fixing sql syntax

--- src/pages/api/[article].js
+++ src/pages/api/[article].js
@@ -3,8 +3,7 @@ import db from "./database/db";
 export default async function handler(req, res) {
   const { article } = req.query
 
-  db.query(
-    "UPDATE articles SET visits=1; WHERE articles_url=?", [article]) 
+  db.query("UPDATE articles SET visits=1 WHERE articles_url=?", [article]) 
 
   db.query("SELECT * FROM articles WHERE articles_url=?", [article], (err, result) => {
     res.status(200).json(result);
