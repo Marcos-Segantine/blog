0e09413684b946439660e52fd0183eb9c7fa2ebf
Marcos Segantine <msegantine8@gmail.com>
2022-12-10 10:46:39 -0300

    Improving UX

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,19 +1,24 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 import header from "../css/layout/header.module.css";
 
 export default function Header() {
+  const router = useRouter()
+
+  console.log(router.pathname);
+
   return (
     <header className={header.header}>
       <nav>
         <ul role="menu">
-          <li className={header.pageSelectedLink}>
-            <Link href="/">Home</Link>
+          <li className={router.pathname === '/' ? header.pageSelectedLink : ''}>
+            <Link href="/" >Home</Link>
           </li>
-          <li>
+          <li className={router.pathname === '/all-articles' ? header.pageSelectedLink : ''}>
             <Link href="/all-articles">Artigos</Link>
           </li>
-          <li>
+          <li className={router.pathname === '/about' ? header.pageSelectedLink : ''}>
             <Link href="/about">Sobre</Link>
           </li>
         </ul>
